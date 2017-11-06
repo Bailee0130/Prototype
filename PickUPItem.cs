9e8480b2f1a703b1ea35c3bab197838480d51ae1
Bailee <33429377+Bailee0130@users.noreply.github.com>
2017-11-06 12:03:41 -0600

    Add files via upload

---
+++ PickUPItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUPItem : MonoBehaviour {
+
+	public bool picked;
+
+	// Use this for initialization
+	void Start () {
+		picked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void isPicked(){
+
+		picked = true;
+	}
+}
---
+++ vr_loo_walk.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vr_loo_walk : MonoBehaviour {
+
+	public Transform vrCamera;
+
+	public float toggleAngle = 20.0f;
+
+	public float speed = 2.0f;
+
+	public bool moveforward;
+
+	private CharacterController cc;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		cc = GetComponent <CharacterController> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0) {
+			moveforward = true;
+		}
+		else {
+			moveforward = false;
+		}
+		if (moveforward) {
+			Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+
+			cc.SimpleMove(forward * speed);
+		}
+	}
+}
